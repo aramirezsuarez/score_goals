ba24588ae0799dadc1a2b62298a6939657fe5767
aramirezsuarez <aramirezsu@unal.edu.co>
2024-01-30 11:23:25 -0500

    Update score_goals.py

--- score_goals.py
+++ score_goals.py
@@ -147,29 +147,3 @@ selected_clubs_info = df_clubs[df_clubs["name"].isin(club_seleccionados)]
 # Mostrar la información en una tabla
 st.write("Información de los clubes seleccionados:")
 st.write(selected_clubs_info)
-
-## Crear un diccionario con la cantidad deseada de jugadores por posición
-posiciones_deseadas = {"Goalkeeper": 1, "Defender": 4, "Midfield": 4, "Attack": 3}
-
-# Inicializar un diccionario para mantener un recuento de jugadores seleccionados por posición
-jugadores_seleccionados_por_posicion = {posicion: 0 for posicion in posiciones_deseadas}
-
-# Crear widgets de selección dinámicamente
-for posicion, cantidad_deseada in posiciones_deseadas.items():
-    jugadores_seleccionados = st.multiselect(
-        f"Selecciona los {posicion.lower()}",
-        df_players[df_players["position"] == posicion]["name"],
-        key=f"{posicion.lower()}_key"
-    )
-
-    # Filtrar el DataFrame para obtener las filas correspondientes a los jugadores seleccionados
-    df_filtrado = df_players[df_players["name"].isin(jugadores_seleccionados)]
-
-    # Contar el número de jugadores seleccionados por posición
-    jugadores_posicion = df_filtrado[df_filtrado["position"] == posicion]
-    jugadores_seleccionados_posicion = min(len(jugadores_posicion), cantidad_deseada)
-    jugadores_seleccionados_por_posicion[posicion] = jugadores_seleccionados_posicion
-
-# Mostrar el resultado
-st.write("Jugadores seleccionados por posición:")
-st.write(jugadores_seleccionados_por_posicion)
