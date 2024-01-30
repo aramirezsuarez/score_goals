ac0c9b81034fe39117fa2675b62621981b840b1d
aramirezsuarez <aramirezsu@unal.edu.co>
2024-01-30 09:34:35 -0500

    Update registro.py

--- pages/registro.py
+++ pages/registro.py
@@ -4,6 +4,7 @@ import streamlit_extras
 import streamlit_authenticator as stauth
 import re
 import pymongo
+from datetime import datetime
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
@@ -24,15 +25,50 @@ def insertar_usuario(email, username, password):
     db.users.insert_one({"key": email, "username": username, "password": password,
             "jugadores": []})
 
-# Formulario para ingresar la información del usuario
-email = st.text_input("Correo Electrónico")
-username = st.text_input("Nombre de Usuario")
-password = st.text_input("Contraseña", type="password")
 
-# Botón para ejecutar la función
-if st.button("Insertar Usuario"):
-    insertar_usuario(email, username, password)
-    st.success("Usuario insertado correctamente en la base de datos.")
+st.title("registro")
+
+# Si se aceptan los términos y condiciones habilitar el registro
+aceptar_terminos = st.checkbox("Acepto los términos y condiciones")
+
+if aceptar_terminos:
+    # Creacion del formulario
+    with st.form(key="registro", clear_on_submit=True):
+        # Titulo del formulario
+        st.subheader("Registrarse")
+
+        # Campos a ser llenados por el usuario
+        email = st.text_input("Email", placeholder="Ingrese su Email")
+        username = st.text_input("Usuario", placeholder="Ingrese su nombre de usuario")
+        password = st.text_input("Contraseña", placeholder="Ingrese su contraseña", type="password")
+
+
+
+        # Boton de envio de datos de registro
+        st.form_submit_button("Registrate")
+
+    # Revisar validez de los datos ingresados por el usuario y registro a la DB
+    if email and username and password:
+        if validar_email(email):
+            if email not in get_emails_usuarios():
+                if validar_username(username):
+                    if username not in get_usernames_usuarios():
+                        password_encriptada = stauth.Hasher([password]).generate()
+                        insertar_usuario(email, username, password_encriptada[0])
+                        st.success("Cuenta creada con éxito!")
+                    else:
+                        st.warning("Nombre de usuario en uso")
+                else:
+                    st.warning("Nombre de usuario inválido (solo debe tener letras y números)")
+            else:
+                st.warning("El email ya está en uso")
+        else:
+            st.warning("Email inválido")
+    else:
+        st.warning("Debe rellenar todos los campos")
+else:
+    st.warning("Debes aceptar los términos y condiciones antes de poder registrarte")
+
 
 
 # Crear pie de pagina con los datos de contacto de los creadores
