2f24080355a60a3f51a90a34d4f06738606e392a
aramirezsuarez <aramirezsu@unal.edu.co>
2024-01-30 08:39:32 -0500

    Update registro.py

--- pages/registro.py
+++ pages/registro.py
@@ -5,6 +5,14 @@ import streamlit_authenticator as stauth
 import re
 import pymongo
 
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return pymongo.MongoClient(**st.secrets["mongo"])
+
+client = init_connection()
+
 # Crear pie de pagina con los datos de contacto de los creadores
 footer = """
 <style>
