4cac8a0ea09080d3ec25399d1df51f35d04f0bcd
aramirezsuarez <aramirezsu@unal.edu.co>
2024-01-30 12:00:48 -0500

    Update arma_tu_equipo.py

--- pages/arma_tu_equipo.py
+++ pages/arma_tu_equipo.py
@@ -44,3 +44,28 @@ if st.session_state["authentication_status"]:
 else:
     st.warning("Si deseas acceder a esta función iniciar sesion")
 
+# Crear pie de pagina con los datos de contacto de los creadores
+footer = """
+<style>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        z-index: 10;
+        width: 100%;
+        background-color: rgb(14, 17, 23);
+        color: black;
+        text-align: center;
+    }
+    .footer p {
+        color: white;
+    }
+</style>
+<div class="footer">
+    <p>App desarrollada por: <br />
+    Andres Felipe Ramirez Suarez <br />
+    Contactenos: <a href="#">aramirezsu@unal.edu.co</a>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
+
